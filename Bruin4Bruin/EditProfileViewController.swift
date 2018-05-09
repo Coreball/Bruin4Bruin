139c590281ec4343a8e2a8095ae5de37645d3da4
Changyuan Lin <coreballium@outlook.com>
2018-05-08 21:03:56 -0600

    Make database document with user's uid when create account, include firstname/lastname/jointime

--- Bruin4Bruin/EditProfileViewController.swift
+++ Bruin4Bruin/EditProfileViewController.swift
@@ -7,13 +7,14 @@
 //
 
 import UIKit
-import FirebaseAuth
+import Firebase
 
 class EditProfileViewController: UIViewController {
 
     @IBOutlet weak var scrollInside: UIView!
     @IBOutlet weak var saveButton: UIBarButtonItem!
     var textFields = [UITextField]()
+    let db = Firestore.firestore()
     var isCreatingAccount = false
     
     override func viewDidLoad() {
@@ -44,6 +45,18 @@ class EditProfileViewController: UIViewController {
                     print("\(type(of: self)) successfully created new user")
                     print("User ID: \(user.uid)")
                     print("Email: \(user.email!)")
+                    self.db.collection("users").document(user.uid).setData([
+                        "first" : self.textFields[3].text!,
+                        "last" : self.textFields[4].text!,
+                        "joined" : Timestamp()
+                        // Submit more things like BIO, etc.
+                    ]) { err in
+                        if let err = err {
+                            print("Error writing document: \(err)")
+                        } else {
+                            print("Successfully wrote to \(user.email!) document")
+                        }
+                    }
                     self.performSegue(withIdentifier: "EditProfileToMessaging", sender: nil)
                 } else {
                     print(error!.localizedDescription)
